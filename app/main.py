4577db184e9f7e7b3357f60e81df46b4b0881fb0
Daniel Mamak <daniel.mamak@gmail.com>
2026-08-08 15:14:45 +0200

    dodaj strukturę klasy Car

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
 class Car:
-    # write your code here
-    pass
+    """Przedstawia samochód i jego parametry"""
+
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
+        try:
+            comfort_class = int(comfort_class)
+            clean_mark = int(clean_mark)
+        except (TypeError, ValueError):
+            raise ValueError(
+                "Klasa komfortu i stopień czystości musi być cyfrą"
+            )
+
+        if not (1 <= comfort_class <= 7):
+            raise ValueError("Podaj zakres od 1 do 7")
+
+        if not (1 <= clean_mark <= 10):
+            raise ValueError("Podaj zakres od 1 do 10")
+
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
