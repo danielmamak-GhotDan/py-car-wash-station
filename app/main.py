3b10feca3aaa60d4d4c6422e7ef317ef252eb2e8
Daniel Mamak <daniel.mamak@gmail.com>
2026-08-08 16:13:16 +0200

    Dodaj strukturę klasy CarWashStation

--- app/main.py
+++ app/main.py
@@ -30,5 +30,35 @@ class Car:
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    """Dane techniczne i pomiarowe myjni"""
+
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
+        try:
+            distance_from_city_center = float(distance_from_city_center)
+            clean_power = int(clean_power)
+            average_rating = float(average_rating)
+            count_of_ratings = int(count_of_ratings)
+        except (TypeError, ValueError):
+            raise ValueError("Wartość podaj w liczbach")
+
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+        if not (1.0 <= distance_from_city_center <= 10.0):
+            raise ValueError("Podaj wartość w zakresie 1.0 - 10.0")
+
+        if not (1.0 <= average_rating <= 5.0):
+            raise ValueError("Podaj wartość w zakresie 1.0 - 5.0")
+
+        self.distance_from_city_center = round(distance_from_city_center, 1)
+        self.clean_power = clean_power
+        self.average_rating = round(average_rating, 1)
+        self.count_of_ratings = count_of_ratings
